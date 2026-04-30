4fc60a20fffd1ec03aabb52d730fb9129aa3f100
Minor Gordon <minorg@server.fake>
2026-04-29 20:20:31 -0400

    generate-workspace-files script

---
+++ generate-workspace-files.ts
@@ -0,0 +1,276 @@
+#!/usr/bin/env npm exec tsx --
+
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+import type { CompilerOptions } from "typescript";
+
+const VERSION = "4.0.15";
+
+const externalDependencies = {
+  "@rdfjs/data-model": "~2.1.1",
+  "@rdfjs/dataset": "~2.0.2",
+  "@rdfjs/term-set": "~2.0.3",
+  "@rdfjs/types": "~2.0.1",
+  "@tpluscode/rdf-ns-builders": "~4.3.0",
+  "@tsconfig/node24": "^24",
+  "@tsconfig/strictest": "~2.0.8",
+  "@types/rdfjs__data-model": "~2.0.9",
+  "@types/rdfjs__dataset": "~2.0.7",
+  "@types/rdfjs__term-set": "~2.0.9",
+  depcheck: "~1.4.7",
+  housemd: "0.1.3",
+  "purify-ts": "~2.1.4",
+  rimraf: "~6.0.1",
+  "ts-invariant": "~0.10.3",
+  typescript: "5.9.3",
+};
+
+type PackageName = "resource";
+
+interface Tsconfig {
+  compilerOptions?: CompilerOptions;
+  // exclude?: string[];
+  extends?: string | string[];
+  // files?: string[];
+  include?: string[];
+  // references?: { path: string; prepend?: boolean }[];
+}
+interface Workspace {
+  bin?: Record<string, string>;
+  dependencies?: {
+    external?: readonly (keyof typeof externalDependencies)[];
+    internal?: readonly PackageName[];
+  };
+  description?: string;
+  devDependencies?: {
+    external?: readonly (keyof typeof externalDependencies)[];
+    internal?: readonly PackageName[];
+  };
+  keywords?: readonly string[];
+  homepage?: string;
+  scripts?: Record<string, string>;
+  tsconfig: Tsconfig;
+}
+
+const packageTsconfig: Tsconfig = {
+  compilerOptions: {
+    declaration: true,
+    declarationMap: true,
+    esModuleInterop: true,
+    exactOptionalPropertyTypes: false,
+    experimentalDecorators: true,
+    forceConsistentCasingInFileNames: true,
+    lib: ["ES2020"],
+    module: "ES2020",
+    moduleResolution: "node",
+    noUncheckedIndexedAccess: false,
+    outDir: "dist",
+    rootDir: "src",
+    sourceMap: true,
+    target: "ES2020",
+  } satisfies CompilerOptions,
+  extends: ["@tsconfig/strictest/tsconfig.json"],
+  include: ["src/**/*.ts"],
+};
+
+const workspaces = {
+  packages: {
+    resource: {
+      dependencies: {
+        external: [
+          "@rdfjs/data-model",
+          "@rdfjs/term-set",
+          "@rdfjs/types",
+          "@types/rdfjs__data-model",
+          "@types/rdfjs__term-set",
+          "purify-ts",
+        ],
+      },
+      devDependencies: {
+        external: [
+          "@rdfjs/dataset",
+          "@tpluscode/rdf-ns-builders",
+          "@types/rdfjs__dataset",
+          "housemd",
+          "ts-invariant",
+        ],
+      },
+      tsconfig: packageTsconfig,
+    },
+  } satisfies Record<PackageName, Workspace>,
+} as const;
+
+const myDirPath = path.dirname(url.fileURLToPath(import.meta.url));
+
+for (const [workspacesDirectoryAny, workspaces_] of Object.entries(
+  workspaces,
+)) {
+  const workspacesDirectoryName = workspacesDirectoryAny as "packages";
+  for (const [workspaceName, workspaceAny] of Object.entries(workspaces_)) {
+    const workspace = workspaceAny as Workspace;
+
+    const packageDirectoryPath = path.join(
+      myDirPath,
+      workspacesDirectoryName,
+      workspaceName,
+    );
+
+    fs.mkdirSync(packageDirectoryPath, { recursive: true });
+
+    const files = new Set<string>();
+    if (fs.existsSync(path.join(packageDirectoryPath, "README.md"))) {
+      files.add("README.md");
+    }
+    const srcDirectoryPath = path.join(packageDirectoryPath, "src");
+    if (fs.existsSync(srcDirectoryPath)) {
+      for (const dirent of fs.readdirSync(srcDirectoryPath, {
+        withFileTypes: true,
+        recursive: true,
+      })) {
+        if (!dirent.name.endsWith(".ts") || !dirent.isFile()) {
+          continue;
+        }
+        for (const fileNameGlob of ["*.js", "*.d.ts"]) {
+          files.add(
+            path.join(
+              "dist",
+              path.relative(srcDirectoryPath, dirent.parentPath),
+              fileNameGlob,
+            ),
+          );
+        }
+      }
+    }
+
+    let testsDirectoryPath: string | null = path.join(
+      packageDirectoryPath,
+      "__tests__",
+    );
+    if (!fs.existsSync(testsDirectoryPath)) {
+      testsDirectoryPath = null;
+    }
+
+    const packageName = `@rdfx/${workspaceName}`;
+
+    fs.writeFileSync(
+      path.join(packageDirectoryPath, "package.json"),
+      `${JSON.stringify(
+        {
+          bin: workspace.bin,
+          dependencies: {
+            ...(workspace.dependencies?.internal ?? []).toSorted().reduce(
+              (map, packageName) => {
+                map[`@rdfx/${packageName}`] = VERSION;
+                return map;
+              },
+              {} as Record<string, string>,
+            ),
+            ...(workspace.dependencies?.external ?? []).toSorted().reduce(
+              (map, packageName) => {
+                map[packageName] = externalDependencies[packageName];
+                return map;
+              },
+              {} as Record<string, string>,
+            ),
+          },
+          description: workspace.description,
+          devDependencies: {
+            ...(workspace.devDependencies?.internal ?? []).toSorted().reduce(
+              (map, packageName) => {
+                map[`@rdfx/${packageName}`] = VERSION;
+                return map;
+              },
+              {} as Record<string, string>,
+            ),
+            ...(workspace.devDependencies?.external ?? [])
+              .concat("@tsconfig/strictest", "depcheck", "rimraf", "typescript")
+              .toSorted()
+              .reduce(
+                (map, packageName) => {
+                  map[packageName] = externalDependencies[packageName];
+                  return map;
+                },
+                {} as Record<string, string>,
+              ),
+          },
+          files: files.size > 0 ? [...files].sort() : undefined,
+          homepage: workspace.homepage,
+          keywords: workspace.keywords,
+          license: "Apache-2.0",
+          main: files.size > 0 ? "./dist/index.js" : undefined,
+          name: packageName,
+          packageManager: "npm@11.11.0",
+          private: workspacesDirectoryName !== "packages" ? true : undefined,
+          repository: {
+            type: "git",
+            url: "git+https://github.com/minorg/rdfx.git",
+          },
+          scripts: {
+            build: `tsc -b${
+              workspace.bin
+                ? ` && ${Object.values(workspace.bin)
+                    .map((bin) => `chmod +x ${bin}`)
+                    .join(" && ")}`
+                : ""
+            }`,
+            clean: "rimraf dist",
+            depcheck: "depcheck .",
+            dev: "tsc -w --preserveWatchOutput",
+            ...(testsDirectoryPath !== null
+              ? {
+                  "dev:tests": "tsc -p __tests__ -w --preserveWatchOutput",
+                }
+              : {}),
+            test: fs.existsSync(path.join(packageDirectoryPath, "__tests__"))
+              ? `cd ../.. && vitest run --project ${packageName}`
+              : undefined,
+            ...workspace.scripts,
+          },
+          type: "module",
+          types: files.size > 0 ? "./dist/index.d.ts" : undefined,
+          version: VERSION,
+        },
+        undefined,
+        2,
+      )}\n`,
+    );
+
+    for (const fileName of ["LICENSE"]) {
+      const packageFilePath = path.resolve(packageDirectoryPath, fileName);
+      if (fs.existsSync(packageFilePath)) {
+        continue;
+      }
+      fs.symlinkSync(`../../${fileName}`, packageFilePath);
+    }
+
+    fs.writeFileSync(
+      path.resolve(packageDirectoryPath, "tsconfig.json"),
+      JSON.stringify(workspace.tsconfig, undefined, 2),
+    );
+
+    if (testsDirectoryPath !== null) {
+      fs.writeFileSync(
+        path.join(testsDirectoryPath, "tsconfig.json"),
+        JSON.stringify(
+          {
+            compilerOptions: {
+              exactOptionalPropertyTypes: false,
+              experimentalDecorators: true,
+              forceConsistentCasingInFileNames: true,
+              noEmit: true,
+              noUncheckedIndexedAccess: false,
+            },
+            extends: [
+              "@tsconfig/node24/tsconfig.json",
+              "@tsconfig/strictest/tsconfig.json",
+            ],
+            include: ["./**/*.ts"],
+          },
+          undefined,
+          2,
+        ),
+      );
+    }
+  }
+}
