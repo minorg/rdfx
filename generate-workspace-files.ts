69e2cac6e29d1f45f70c1b7a1ce4f6e13a880db4
Minor Gordon <minorg@server.fake>
2026-05-03 16:40:07 -0400

    bump vtersion to test CD

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import url from "node:url";
 import type { CompilerOptions } from "typescript";
 
-const VERSION = "0.0.4";
+const VERSION = "0.0.5";
 
 const vitestVersion = "~4.1.5";
 
