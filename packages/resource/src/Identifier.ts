b0316a1ab1f1da8b97e3e88a327c9d7154d48c3f
Minor Gordon <minorg@server.fake>
2026-05-05 13:11:26 -0400

    remove Resource.Identifier.fromString

--- packages/resource/src/Identifier.ts
+++ packages/resource/src/Identifier.ts
@@ -1,27 +1,3 @@
-import type { BlankNode, DataFactory, NamedNode } from "@rdfjs/types";
+import type { BlankNode, NamedNode } from "@rdfjs/types";
 
 export type Identifier = BlankNode | NamedNode;
-
-export namespace Identifier {
-  export function fromString({
-    dataFactory,
-    identifier,
-  }: {
-    dataFactory: DataFactory;
-    identifier: string;
-  }) {
-    if (identifier.startsWith("_:")) {
-      return dataFactory.blankNode(identifier.substring("_:".length));
-    }
-    if (
-      identifier.startsWith("<") &&
-      identifier.endsWith(">") &&
-      identifier.length > 2
-    ) {
-      return dataFactory.namedNode(
-        identifier.substring(1, identifier.length - 1),
-      );
-    }
-    throw new RangeError(identifier);
-  }
-}
