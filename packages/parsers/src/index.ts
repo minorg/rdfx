b9a5a8aedd00359f260350d6e889ef4977421be3
Minor Gordon <minorg@server.fake>
2026-05-18 19:48:44 -0400

    parsers index

---
+++ packages/parsers/src/index.ts
@@ -0,0 +1,22 @@
+import JsonLdParser, { type ParserOptions } from "@rdfjs/parser-jsonld";
+import N3Parser from "@rdfjs/parser-n3";
+import SinkMap from "@rdfjs/sink-map";
+import type { DataFactory } from "@rdfjs/types";
+
+export default function parsers({
+  dataFactory,
+  ...otherOptions
+}: { dataFactory: DataFactory } & Omit<ParserOptions, "factory">) {
+  const options = { ...otherOptions, factory: dataFactory };
+
+  const n3Parser = new N3Parser(options);
+
+  return new SinkMap([
+    ["application/ld+json", new JsonLdParser(options)],
+    ["application/trig", n3Parser],
+    ["application/n-quads", n3Parser],
+    ["application/n-triples", n3Parser],
+    ["text/n3", n3Parser],
+    ["text/turtle", n3Parser],
+  ]);
+}
