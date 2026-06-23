95ebdeb82b5b7867f1c525b0b1a57f1f84ef789e
Minor Gordon <minorg@server.fake>
2026-06-23 18:07:24 -0400

    regenerate workspace files

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -510,7 +510,7 @@ fs.writeFileSync(
         "check:write:unsafe": "biome check --write --unsafe",
         clean: "turbo run clean",
         depcheck: "turbo run depcheck",
-        dev: "turbo run --concurrency 18 dev dev:tests",
+        dev: "turbo run --concurrency 20 dev dev:tests",
         test: "vitest run",
         "test:coverage": "vitest run --coverage",
       },
