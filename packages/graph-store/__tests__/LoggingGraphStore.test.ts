d0f40fdb9c9f001d6b2dbcf6e66e7a78c265768d
Minor Gordon <minorg@server.fake>
2026-06-24 14:24:57 -0400

    LoggingGraphStore

---
+++ packages/graph-store/__tests__/LoggingGraphStore.test.ts
@@ -0,0 +1,17 @@
+import datasetFactory from "@rdfjs/dataset";
+import { dummyLogger } from "ts-log";
+import { describe } from "vitest";
+import { LoggingGraphStore } from "../src/LoggingGraphStore.js";
+import { RdfjsDatasetGraphStore } from "../src/RdfjsDatasetGraphStore.js";
+import { testGraphStore } from "./testGraphStore.js";
+
+describe("LoggingGraphStore", () => {
+  testGraphStore((use) =>
+    use(
+      new LoggingGraphStore(
+        new RdfjsDatasetGraphStore(datasetFactory.dataset()),
+        dummyLogger,
+      ),
+    ),
+  );
+});
--- packages/graph-store/src/index.ts
+++ packages/graph-store/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./GraphStore.js";
+export * from "./LoggingGraphStore.js";
+export * from "./RdfjsDatasetGraphStore.js";
+export * from "./VersionedGraphStore.js";
