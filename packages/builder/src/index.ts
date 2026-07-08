111a4db8ad7a9c2baf4bd0748f52bfda16e6edd1
Minor Gordon <minorg@server.fake>
2026-07-08 14:35:11 -0400

    re-export shapes

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -3,7 +3,32 @@ export { builder as default } from "./builder.js";
 import type { DatasetCore } from "@rdfjs/types";
 import dataFactory from "@rdfx/data-factory";
 import { ResourceSet } from "@rdfx/resource";
-import { $Object } from "./shapes.js";
+import {
+  $Object,
+  type owl_Ontology,
+  type sh_NodeShape,
+  type sh_PropertyGroup,
+  type sh_PropertyShape,
+  type sh_Shape,
+  type skos_Concept,
+  type skos_ConceptScheme,
+} from "./shapes.js";
+
+export namespace owl {
+  export type Ontology = owl_Ontology;
+}
+
+export namespace sh {
+  export type NodeShape = sh_NodeShape;
+  export type PropertyGroup = sh_PropertyGroup;
+  export type PropertyShape = sh_PropertyShape;
+  export type Shape = sh_Shape;
+}
+
+export namespace skos {
+  export type Concept = skos_Concept;
+  export type ConceptScheme = skos_ConceptScheme;
+}
 
 export function toRdfDataset(
   dataset: DatasetCore,
