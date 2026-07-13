8a0ecf54f41dcb8aef377fa6d184acc8d8d6454a
Minor Gordon <minorg@server.fake>
2026-07-13 14:51:32 -0400

    refining test

--- packages/git/__tests__/VersionedRdfDirectoryGraphStore.test.ts
+++ packages/git/__tests__/VersionedRdfDirectoryGraphStore.test.ts
@@ -1,4 +1,4 @@
-import fs from "node:fs/promises";
+import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import { testVersionedGraphStore } from "@rdfx/graph-store/__tests__/testVersionedGraphStore.js";
@@ -9,12 +9,12 @@ import { logger } from "./logger.js";
 
 describe("VersionedRdfDirectoryGraphStore", () => {
   testVersionedGraphStore(async (use) => {
-    await using directoryPath = await fs.mkdtempDisposable(
+    await using directoryPath = await fs.promises.mkdtempDisposable(
       path.join(os.tmpdir(), "VersionedRdfDirectoryGraphStore.test"),
     );
     await git.init({ fs, dir: directoryPath.path });
 
-    const ignoreFileHandle = await fs.open(
+    const ignoreFileHandle = await fs.promises.open(
       path.join(directoryPath.path, "README.md"),
       "w+",
     );
