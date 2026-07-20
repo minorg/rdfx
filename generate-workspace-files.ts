eaa38ce9a03319bfc586f0fa38e8412c50a7ca88
Minor Gordon <minorg@server.fake>
2026-07-20 17:34:45 -0400

    bump version and shaclmate version

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -5,9 +5,9 @@ import path from "node:path";
 import url from "node:url";
 import type { CompilerOptions } from "typescript";
 
-const VERSION = "0.0.42";
+const VERSION = "0.0.43";
 
-const shaclmateVersion = "4.0.65";
+const shaclmateVersion = "4.0.77";
 const vitestVersion = "~4.1.5";
 
 const externalDependencies = {
