2b064ea66a66cca7ec358c3ff74b59b56b7198ea
Minor Gordon <minorg@server.fake>
2026-08-09 17:32:41 -0400

    bump dev concurrency

--- generate-workspace-files.ts
+++ generate-workspace-files.ts
@@ -581,7 +581,7 @@ fs.writeFileSync(
         "check:write:unsafe": "biome check --write --unsafe",
         clean: "turbo run clean",
         depcheck: "turbo run depcheck",
-        dev: "turbo run --concurrency 25 dev dev:tests",
+        dev: "turbo run --concurrency 26 dev dev:tests",
         test: "vitest run",
         "test:coverage": "vitest run --coverage",
       },
